fca7eca4b9dbd8a9fa3479fb28ec3bbace716468
ammarahm-ed <ammarahmed6506@gmail.com>
2020-03-09 11:53:17 +0500

    add wrapper for getting and setting an array

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 import { NativeModules } from "react-native";
 
-const { RNFastStorage } = NativeModules;
+const RNFastStorage = NativeModules.RNFastStorage;
 
 if (RNFastStorage.setupLibrary) RNFastStorage.setupLibrary();
+/**
+ * Set an array to the db.
+ * @param {String} key 
+ * @param {Array} array 
+ */
+export async function setArray(key, array) {
+  let data = {};
+  data[key] = array;
+  await RNFastStorage.setMap(key, data);
+  return true;
+}
+
+/**
+ * get an array from the db.
+ * @param {String} key 
+ */
+
+export async function getArray(key) {
+  let data = await RNFastStorage.getMap(key);
+  return data[key];
+}
+
+
 
 export default RNFastStorage;
