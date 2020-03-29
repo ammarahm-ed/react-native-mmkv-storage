95b58027d565b310c13e3063eccb6802d1740d82
ammarahm-ed <ammarahmed6506@gmail.com>
2020-03-29 15:26:18 +0500

    Channel everything through single import

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 
 
-const MMKVLoader = require('./src/loader').default;
+const Loader = require('./src/loader').default;
 
-export default MMKVLoader;
+const MMKVStorage = {
+  Loader:Loader,
+  MODES: {
+    SINGLE_PROCESS:1,
+    MULTI_PROCESS:2
+  },
+  ACCESSIBLE :{
+    WHEN_UNLOCKED                      : 'AccessibleWhenUnlocked',
+    AFTER_FIRST_UNLOCK                 : 'AccessibleAfterFirstUnlock',
+    ALWAYS                             : 'AccessibleAlways',
+    WHEN_PASSCODE_SET_THIS_DEVICE_ONLY : 'AccessibleWhenPasscodeSetThisDeviceOnly',
+    WHEN_UNLOCKED_THIS_DEVICE_ONLY     : 'AccessibleWhenUnlockedThisDeviceOnly',
+    AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY: 'AccessibleAfterFirstUnlockThisDeviceOnly',
+    ALWAYS_THIS_DEVICE_ONLY            : 'AccessibleAlwaysThisDeviceOnly',
+  }
+}
 
+export default MMKVStorage;
 
