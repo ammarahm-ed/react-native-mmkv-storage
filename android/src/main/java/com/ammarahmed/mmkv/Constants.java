38ac5d82d6b1735a676ade056c148e46e4081d94
ammarahm-ed <ammarahmed6506@gmail.com>
2020-03-29 15:27:14 +0500

    add secure storage on android

---
+++ android/src/main/java/com/ammarahmed/mmkv/Constants.java
@@ -0,0 +1,19 @@
+package com.ammarahmed.mmkv;
+
+public class Constants {
+
+    // Key Store
+    public static final String KEYSTORE_PROVIDER_1 = "AndroidKeyStore";
+    public static final String KEYSTORE_PROVIDER_2 = "AndroidKeyStoreBCWorkaround";
+    public static final String KEYSTORE_PROVIDER_3 = "AndroidOpenSSL";
+
+    public static final String RSA_ALGORITHM = "RSA/ECB/PKCS1Padding";
+    public static final String AES_ALGORITHM = "AES/ECB/PKCS5Padding";
+
+    public static final String TAG = "RNSecureStorage";
+
+    // Internal storage file
+    public static final String SKS_KEY_FILENAME = "SKS_KEY_FILE";
+    public static final String SKS_DATA_FILENAME = "SKS_DATA_FILE";
+
+}
