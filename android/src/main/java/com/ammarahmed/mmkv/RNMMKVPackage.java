dcc3b39dd0b899f652533e5516e2ab457db4fd40
ammarahm-ed <ammarahmed6506@gmail.com>
2020-03-29 15:28:44 +0500

    implement encryption & multi instance support

---
+++ android/src/main/java/com/ammarahmed/mmkv/RNMMKVPackage.java
@@ -0,0 +1,29 @@
+
+package com.ammarahmed.mmkv;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+
+public class RNMMKVPackage implements ReactPackage {
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Arrays.<NativeModule>asList(new RNMMKVModule(reactContext));
+  }
+
+  // Deprecated from RN 0.47
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+}
