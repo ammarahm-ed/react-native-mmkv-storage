de66ad0cdce7bcf09b1d6374d3007a64e4e25068
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-05 11:00:44 +0500

    prevent extra cycles

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
+import { ACCESSIBLE, MODES } from 'react-native-mmkv-storage/src/utils';
 
 
 const Loader = require('./src/loader').default;
 
 const MMKVStorage = {
   Loader:Loader,
-  MODES: {
-    SINGLE_PROCESS:1,
-    MULTI_PROCESS:2
-  },
-  ACCESSIBLE :{
-    WHEN_UNLOCKED                      : 'AccessibleWhenUnlocked',
-    AFTER_FIRST_UNLOCK                 : 'AccessibleAfterFirstUnlock',
-    ALWAYS                             : 'AccessibleAlways',
-    WHEN_PASSCODE_SET_THIS_DEVICE_ONLY : 'AccessibleWhenPasscodeSetThisDeviceOnly',
-    WHEN_UNLOCKED_THIS_DEVICE_ONLY     : 'AccessibleWhenUnlockedThisDeviceOnly',
-    AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY: 'AccessibleAfterFirstUnlockThisDeviceOnly',
-    ALWAYS_THIS_DEVICE_ONLY            : 'AccessibleAlwaysThisDeviceOnly',
-  }
+  MODES: MODES,
+  ACCESSIBLE : ACCESSIBLE
 }
 
 export default MMKVStorage;
