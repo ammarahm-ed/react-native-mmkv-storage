7b67d203fc162ca95ae33a9277d6884852936bf5
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-05 11:01:49 +0500

    remove helpers.js & use utils.js

--- src/helpers.js
+++
@@ -1,8 +0,0 @@
-
-export const stringToHex = (input) => {
-  let str = ""
-  for (const char of input) {
-    str += char.charCodeAt(0).toString(16)
-  }
-  return str
-}
---
+++ src/utils.js
@@ -0,0 +1,56 @@
+
+
+
+export function promisify(fn) {
+
+  return function(...args) {
+
+    return new Promise((resolve,reject) => {
+
+
+      fn(...args,(error,result) => {
+
+        if (error){
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      })
+
+    })
+
+  }
+
+}
+
+
+export const ACCESSIBLE = {
+  WHEN_UNLOCKED                      : 'AccessibleWhenUnlocked',
+  AFTER_FIRST_UNLOCK                 : 'AccessibleAfterFirstUnlock',
+  ALWAYS                             : 'AccessibleAlways',
+  WHEN_PASSCODE_SET_THIS_DEVICE_ONLY : 'AccessibleWhenPasscodeSetThisDeviceOnly',
+  WHEN_UNLOCKED_THIS_DEVICE_ONLY     : 'AccessibleWhenUnlockedThisDeviceOnly',
+  AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY: 'AccessibleAfterFirstUnlockThisDeviceOnly',
+  ALWAYS_THIS_DEVICE_ONLY            : 'AccessibleAlwaysThisDeviceOnly',
+}
+
+export const MODES = {
+  SINGLE_PROCESS:1,
+  MULTI_PROCESS:2
+}
+
+export const DATA_TYPES = Object.freeze({
+  STRING: 1,
+  NUMBER: 2,
+  BOOL: 3,
+  MAP: 4,
+  ARRAY: 5
+});
+
+export const stringToHex = (input) => {
+  let str = ""
+  for (const char of input) {
+    str += char.charCodeAt(0).toString(16)
+  }
+  return str
+}
