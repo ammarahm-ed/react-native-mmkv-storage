60bc4ed687cedb7ead65a5f88e8120f36a6c4e9e
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-05 18:11:58 +0500

    add new methods

--- android/src/main/java/com/ammarahmed/mmkv/IDStore.java
+++ android/src/main/java/com/ammarahmed/mmkv/IDStore.java
@@ -1,8 +1,17 @@
 package com.ammarahmed.mmkv;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 import com.tencent.mmkv.MMKV;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class IDStore {
@@ -16,22 +25,88 @@ public class IDStore {
     }
 
 
-    public void add(String ID) {
+    public void add(String ID, boolean encrypted, @Nullable String alias) {
 
         boolean hasKey = store.containsKey("mmkvIdStore");
-
+        HashMap<String,Object> IdStore =  new HashMap<>();
         if (hasKey) {
-            Set<String> IdStore = new HashSet<>();
-            IdStore = store.decodeStringSet("mmkvIdStore", IdStore);
-            IdStore.add(ID);
-            store.encode("mmkvIdStore", IdStore);
-        } else {
-            Set<String> IdStore = new HashSet<>();
-            IdStore.add(ID);
-            store.encode("mmkvIdStore", IdStore);
+            Bundle mmkvIdStore = store.decodeParcelable("mmkvIdStore",Bundle.class);
+           IdStore =  (HashMap<String, Object>) mmkvIdStore.getSerializable("mmkvIdStore");
+
         }
 
+        HashMap<String,Object> child = new HashMap<>();
+        child.put("ID",ID);
+        child.put("encrypted",encrypted);
+        child.put(alias,alias);
+        IdStore.put(ID,child);
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("mmkvIdStore",IdStore);
+
+        store.encode("mmkvIdStore", bundle);
+
     }
+
+    public boolean encrypted(String ID) {
+
+        boolean hasKey = store.containsKey("mmkvIdStore");
+        HashMap<String,Object> IdStore =  new HashMap<>();
+
+        if (!hasKey) {
+            return false;
+        }
+        Bundle mmkvIdStore = store.decodeParcelable("mmkvIdStore",Bundle.class);
+        IdStore =  (HashMap<String, Object>) mmkvIdStore.getSerializable("mmkvIdStore");
+
+        HashMap<String,Object> child = (HashMap<String, Object>) IdStore.get(ID);
+
+
+        return  (boolean) child.get("encrypted");
+
+
+    }
+
+    public String getAlias(String ID) {
+
+        boolean hasKey = store.containsKey("mmkvIdStore");
+        HashMap<String,Object> IdStore =  new HashMap<>();
+
+        if (!hasKey) {
+            return null;
+        }
+        Bundle mmkvIdStore = store.decodeParcelable("mmkvIdStore",Bundle.class);
+        IdStore =  (HashMap<String, Object>) mmkvIdStore.getSerializable("mmkvIdStore");
+
+        HashMap<String,Object> child = (HashMap<String, Object>) IdStore.get(ID);
+
+        return  (String) child.get("alias");
+
+    }
+
+
+    public HashMap<String,Object> getAll() {
+
+        boolean hasKey = store.containsKey("mmkvIdStore");
+        HashMap<String,Object> IdStore =  new HashMap<>();
+
+        if (!hasKey) {
+            return null;
+        }
+        Bundle mmkvIdStore = store.decodeParcelable("mmkvIdStore",Bundle.class);
+        IdStore =  (HashMap<String, Object>) mmkvIdStore.getSerializable("mmkvIdStore");
+
+
+
+        return  IdStore;
+
+    }
+
+
+
+
+
+
 
 
     public boolean exists(String ID) {
