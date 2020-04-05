031a7a99fa616164cd78761a15d834cdbcffdbca
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-05 11:00:27 +0500

    cleanup api

--- src/api.js
+++ src/api.js
@@ -1,169 +1,82 @@
-import generatePassword from "react-native-mmkv-storage/src/keygen";
-import { stringToHex } from "react-native-mmkv-storage/src/helpers";
-import MMKVStorage from "react-native-mmkv-storage";
 import encryption from "react-native-mmkv-storage/src/encryption";
+import { promisify } from "react-native-mmkv-storage/src/utils";
 import indexer from "react-native-mmkv-storage/src/indexer";
-
-
-const DATA_TYPES = Object.freeze({
-    STRING:1,
-    NUMBER:2,
-    BOOL:3,
-    MAP:4,
-    ARRAY:5
-});
-
-
+import { DATA_TYPES, ACCESSIBLE } from "./utils";
 
 export default class API {
-  constructor({id = "default", mmkv,alias,aliasPrefix,key }) {
-    
+  constructor({ id = "default", mmkv, alias, aliasPrefix, key, accessibleMode = ACCESSIBLE.WHEN_UNLOCKED }) {
     this.MMKV = mmkv;
     this.instanceID = id;
     this.alias = alias;
-    this.aliasPrefix = aliasPrefix
+    this.aliasPrefix = aliasPrefix;
     this.key = key;
-    let options = {id:this.instanceID, mmkv:this.MMKV, alias:this.alias, aliasPrefix:this.aliasPrefix,key:this.key}
+    let options = {
+      id: this.instanceID,
+      mmkv: this.MMKV,
+      alias: this.alias,
+      aliasPrefix: this.aliasPrefix,
+      key: this.key,
+      accessibleMode:accessibleMode
+    };
     this.encryption = new encryption(options);
     this.indexer = new indexer(options);
- 
-  }
-  /**
-   * Set a string value to storage for a given key.
-   *
-   * @param {String} key
-   * @param {String} value
-   *
-   */
-
-  async setStringAsync(key, value) {
-    if (typeof value !== "string")
-      throw new Error("The provided value is not a string");
-
-    return await this.MMKV.setStringAsync(this.instanceID, key, value);
   }
 
-  /**
-   * Get a string value for a given key.
-   * @param {String} key
-   */
+  setStringAsync(key, value) {
+    return promisify(this.setString)(this.instanceID, key, value);
+  }
+
   async getStringAsync(key) {
-    return await this.MMKV.getItemAsync(this.instanceID, key, DATA_TYPES.STRING);
+    return promisify(this.setString)(this.instanceID, key, DATA_TYPES.STRING);
   }
 
-  /**
-   * Set a number value to storage for a given key.
-   *
-   * @param {String} key
-   * @param {number} value
-   *
-   */
-  async setIntAsync(key, value) {
-    if (isNaN(value)) throw new Error("The provided value is not a number");
-    return await this.MMKV.setIntAsync(this.instanceID, key, value);
+  setIntAsync(key, value) {
+    return promisify(this.setInt)(this.instanceID, key, value);
   }
 
-  /**
-   * Get a number value for a given key
-   * @param {String} key
-   */
-  async getIntAsync(key) {
-    return await this.MMKV.getItemAsync(this.instanceID, key, DATA_TYPES.NUMBER);
+  getIntAsync(key) {
+    return promisify(this.setString)(this.instanceID, key, DATA_TYPES.NUMBER);
   }
 
-  /**
-   * Set a boolean value to storag for a given key.
-   *
-   * @param {String} key
-   * @param {boolean} value
-   *
-   */
-  async setBoolAsync(key, value) {
-    if (typeof value !== "boolean")
-      throw new Error("The provided value is not a boolean");
-    return await this.MMKV.setBoolAsync(this.instanceID, key, value);
+  setBoolAsync(key, value) {
+    return promisify(this.setBool)(this.instanceID, key, value);
   }
 
-  /**
-   * Get a boolean value for a given key.
-   * @param {String} key
-   */
-  async getBoolAsync(key) {
-    return await this.MMKV.getItemAsync(this.instanceID, key, DATA_TYPES.BOOL);
+  setMapAsync(key, value) {
+    return promisify(this.setMap)(this.instanceID, key, value);
   }
 
-  /**
-   * Set an Object to storage for a given key.
-   *
-   * @param {String} key
-   * @param {Object} value
-   *
-   */
-  async setMapAsync(key, value) {
-    if (typeof value !== "object")
-      throw new Error("The provided value is not a object");
-    return await this.MMKV.setMapAsync(this.instanceID, key, value,false);
+  getMapAsync(key) {
+    return promisify(this.setString)(this.instanceID, key, DATA_TYPES.MAP);
   }
 
-  /**
-   * Get an Object from storage for a given key.
-   * @param {String} key
-   */
-  async getMapAsync(key) {
-    return await this.MMKV.getItemAsync(this.instanceID, key, DATA_TYPES.MAP);
-  }
-
-  /**
-   * Remove an item from storage for a given key.
-   *
-   * @param {String} key
-   */
   async removeItem(key) {
     return await this.MMKV.removeItem(this.instanceID, key);
   }
-
-  /**
-   * Clear the storage.
-   *
-   */
 
   async clearStore() {
     return await this.MMKV.clearStore(this.instanceID);
   }
 
- 
-
-  /**
-   * Retrieve multiple Objects for a given array of keys. Currently will work only if data for all keys is an Object.
-   * Arrays will also be returned but wrappen in a object.
-   *
-   * @param {Array} keys
-   */
   async getMultipleItemsAsync(keys) {
-    return await this.MMKV.getMultipleItemsAsync(this.instanceID, keys);
+    return promisify(this.getMultipleItems)(this.instanceID, keys, value);
   }
 
-  /**
-   * Set an array to the db.
-   * @param {String} key
-   * @param {Array} array
-   */
   async setArrayAsync(key, array) {
     if (!Array.isArray(array))
       throw new Error("Provided value is not an array");
     let data = {};
     data[key] = array.slice();
 
-    return await this.MMKV.setMapAsync(this.instanceID, key, data,true);
+    return await this.MMKV.setMapAsync(this.instanceID, key, data, true);
   }
 
-  /**
-   * get an array from the storage for give key.
-   * @param {String} key
-   */
-
   async getArrayAsync(key) {
-    let data = await this.MMKV.getItemAsync(this.instanceID, key,DATA_TYPES.ARRAY);
+    let data = await this.MMKV.getItemAsync(
+      this.instanceID,
+      key,
+      DATA_TYPES.ARRAY
+    );
     if (data) {
       return data[key].slice();
     } else {
@@ -171,134 +84,55 @@ export default class API {
     }
   }
 
-  // NO ASYNC CALLS
-
-  /**
-   * Set a string value to storage for a given key.
-   *
-   * @param {String} key
-   * @param {String} value
-   * @param {Function} callback
-   *
-   */
   setString(key, value, callback) {
-    if (typeof value !== "string")
-      throw new Error("The provided value is not a string");
-
     return this.MMKV.setString(this.instanceID, key, value, callback);
   }
 
-  /**
-   * Get a string value for a given key.
-   * @param {String} key
-   * @param {Function} callback
-   */
   getString(key, callback) {
-    return this.MMKV.getItem(this.instanceID, key,DATA_TYPES.STRING, callback);
+    return this.MMKV.getItem(this.instanceID, key, DATA_TYPES.STRING, callback);
   }
 
-  /**
-   * Set a number value to storage for a given key.
-   *
-   * @param {String} key
-   * @param {number} value
-   * @param {Function} callback
-   */
   setInt(key, value, callback) {
-    if (isNaN(value)) throw new Error("The provided value is not a number");
     return this.MMKV.setInt(this.instanceID, key, value, callback);
   }
 
-  /**
-   * Get a number value for a given key
-   * @param {String} key
-   * @param {Function} callback
-   */
   getInt(key, callback) {
-    return this.MMKV.getItem(this.instanceID, key,DATA_TYPES.NUMBER, callback);
+    return this.MMKV.getItem(this.instanceID, key, DATA_TYPES.NUMBER, callback);
   }
 
-  /**
-   * Set a boolean value to storag for a given key.
-   *
-   * @param {String} key
-   * @param {boolean} value
-   * @param {Function} callback
-   *
-   */
   setBool(key, value, callback) {
-    if (typeof value !== "boolean")
-      throw new Error("The provided value is not a boolean");
+
     return this.MMKV.setBool(this.instanceID, key, value, callback);
   }
 
-  /**
-   * Get a boolean value for a given key.
-   * @param {String} key
-   * @param {Function} callback
-   */
   getBool(key, callback) {
-    return this.MMKV.getItem(this.instanceID, key,DATA_TYPES.BOOL, callback);
+    return this.MMKV.getItem(this.instanceID, key, DATA_TYPES.BOOL, callback);
   }
 
-  /**
-   * Set an Object to storage for a given key.
-   *
-   * @param {String} key
-   * @param {Object} value
-   * @param {Function} callback
-   *
-   */
   setMap(key, value, callback) {
-    if (typeof value !== "object")
-      throw new Error("The provided value is not a object");
-    return this.MMKV.setMap(this.instanceID, key, value,false, callback);
+  
+    return this.MMKV.setMap(this.instanceID, key, value, false, callback);
   }
 
-  /**
-   * Get an Object from storage for a given key.
-   * @param {String} key
-   * @param {Function} callback
-   */
   getMap(key, callback) {
-    return this.MMKV.getItem(this.instanceID, key,DATA_TYPES.MAP, callback);
+    return this.MMKV.getItem(this.instanceID, key, DATA_TYPES.MAP, callback);
   }
 
-
-  /**
-   * Retrieve multiple Objects for a given array of keys. Currently will work only if data for all keys is an Object.
-   * Arrays will also be returned but wrappen in a object.
-   *
-   * @param {Array} keys
-   * @param {Function} callback
-   */
   getMultipleItems(keys, callback) {
     return this.MMKV.getMultipleItems(this.instanceID, keys, callback);
   }
 
-  /**
-   * Set an array to the db.
-   * @param {String} key
-   * @param {Array} array
-   * @param {Function} callback
-   */
   setArray(key, array, callback) {
     if (!Array.isArray(array))
       throw new Error("Provided value is not an array");
     let data = {};
     data[key] = array.slice();
 
-    return this.MMKV.setMap(this.instanceID, key, data,true, callback);
+    return this.MMKV.setMap(this.instanceID, key, data, true, callback);
   }
 
-  /**
-   * get an array from the storage for give key.
-   * @param {String} key
-   * @param {Function} callback
-   */
-
   getArray(key, callback) {
-    this.MMKV.getItem(this.instanceID, key,DATA_TYPES.ARRAY, data => {
+    this.MMKV.getItem(this.instanceID, key, DATA_TYPES.ARRAY, (data) => {
       if (data) {
         return callback(data[key].slice());
       } else {
@@ -308,20 +142,10 @@ export default class API {
   }
 
   async getCurrentMMKVInstanceIDs() {
-
-
-  return await this.MMKV.getCurrentMMKVInstanceIDs()
-
-
+    return await this.MMKV.getCurrentMMKVInstanceIDs();
   }
 
   async getAllMMKVInstanceIDs() {
-
-
-    return await this.MMKV.getAllMMKVInstanceIDs()
-  
-  
-    }
-
-
+    return await this.MMKV.getAllMMKVInstanceIDs();
+  }
 }
