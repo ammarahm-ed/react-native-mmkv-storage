7128d3132c885bb8f2b4d1b55b9d693f8c569918
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-22 15:22:17 +0500

    wrap root indexer in action handlers

--- src/indexer/indexer.js
+++ src/indexer/indexer.js
@@ -3,22 +3,24 @@ import numbersIndex from "./numbers";
 import boolIndex from "./booleans";
 import mapsIndex from "./maps";
 import arrayIndex from "./arrays";
- 
+import { handleActionAsync } from "../handlers";
 export default class indexer {
-
-  constructor({ id = "default", mmkv }) {
+  constructor(args) {
     this.MMKV = mmkv;
     this.instanceID = id;
-    this.strings = new stringsIndex({ id: id, mmkv: mmkv });
-    this.numbers = new numbersIndex({ id: id, mmkv: mmkv });
-    this.booleans = new boolIndex({ id: id, mmkv: mmkv });
-    this.maps = new mapsIndex({ id: id, mmkv: mmkv });
-    this.arrays = new arrayIndex({ id: id, mmkv: mmkv });
+    this.options = args;
+    this.strings = new stringsIndex({ id: args.id, mmkv: args.mmkv });
+    this.numbers = new numbersIndex({ id: args.id, mmkv: args.mmkv });
+    this.booleans = new boolIndex({ id: args.id, mmkv: args.mmkv });
+    this.maps = new mapsIndex({ id: args.id, mmkv: args.mmkv });
+    this.arrays = new arrayIndex({ id: args.id, mmkv: args.mmkv });
   }
+
   async getKeys() {
-    return await this.MMKV.getKeys(this.instanceID);
+    return await handleActionAsync(this.MMKV.getKeys, this.instanceID);
   }
+
   async hasKey(key) {
-    return await this.MMKV.hasKey(this.instanceID, key);
+    return await handleActionAsync(this.MMKV.hasKey, this.instanceID, key);
   }
 }
