7cfdc0b3c1b63e068d38fbe66d7eaf691a007ebb
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-22 18:58:27 +0500

    fix handleActionAsync

--- src/handlers.js
+++ src/handlers.js
@@ -1,12 +1,12 @@
 /**
- * 
+ *
  * A handler function used to handle all the
  * calls made to native code. The purpose is
  * to make sure that the storage is initialized
  * before any storage requests are sent to the
- * MMKV instance. 
- * 
- * 
+ * MMKV instance.
+ *
+ *
  * @param {boolean} initialized Tells the function if the storage has been loaded
  * @param {*} options The options you used to initialize the loader class
  * @param {*} action The native function that will be called
@@ -18,40 +18,43 @@ import { currentInstancesStatus } from "./initializer";
 
 export function handleAction(action, callback, ...args) {
   if (currentInstancesStatus[this.instanceID]) {
-   return action(...args, callback);
-  
+    return action(...args, callback);
   } else {
     initialize(this.options, (err, result) => {
       if (err) {
-        currentInstancesStatus[this.instanceID] = false
-       return callback(err, null);
-       
+        currentInstancesStatus[this.instanceID] = false;
+        return callback(err, null);
       }
       currentInstancesStatus[this.instanceID] = true;
-     return action(...args, callback);
-    
+      return action(...args, callback);
     });
   }
 }
 
+export async function handleActionAsync(action, ...args) {
+  return new Promise(async (resolve, reject) => {
+    if (currentInstancesStatus[this.instanceID]) {
+      try {
+        let result = await action(...args);
+        resolve(result);
+      } catch (e) {
+        reject(e);
+      }
+    } else {
+      initialize(this.options, async (err, result) => {
+        if (err) {
+          currentInstancesStatus[this.instanceID] = false;
+          return reject(err);
+        }
 
-export async function handleActionAsync(action,...args) {
-
-  if (currentInstancesStatus[this.instanceID]) {
-    return await action(...args);
-   
-   } else {
-     initialize(this.options, (err, result) => {
-       if (err) {
-        currentInstancesStatus[this.instanceID] = false
-        return await action(...args);
-        
-       }
-       currentInstancesStatus[this.instanceID] = true;
-      return await action(...args);
-     
-     });
-   }
-
-
+        currentInstancesStatus[this.instanceID] = true;
+        try {
+          let result = await action(...args);
+          resolve(result);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    }
+  });
 }
