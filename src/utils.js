dd9c7ee92668438ef8c76c4d2a6783fbb69dca26
ammarahm-ed <ammarahmed6506@gmail.com>
2020-10-28 20:04:26 +0500

    promise will always resolve with a fallback value

--- src/utils.js
+++ src/utils.js
@@ -5,12 +5,7 @@ export function promisify(fn) {
     return new Promise((resolve, reject) => {
 
       fn(...args, (error, result) => {
-
-        if (error) {
-          reject(error);
-        } else {
           resolve(result);
-        }
       })
 
     })
