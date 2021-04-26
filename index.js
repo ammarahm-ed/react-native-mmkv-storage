02b57f30940be35fcbc25f76715eae60ae7be88c
ammarahm-ed <ammarahmed6506@gmail.com>
2021-04-27 00:58:43 +0500

    add type definitions

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+import { NativeModules } from 'react-native';
 import { ACCESSIBLE, MODES } from 'react-native-mmkv-storage/src/utils';
-
+if (!global.getStringMMKV) {
+  NativeModules.MMKVNative.installMMKV()
+}
 const Loader = require('./src/loader').default;
 
 const MMKVStorage = {
   Loader:Loader,
   MODES: MODES,
-  ACCESSIBLE : ACCESSIBLE
+  ACCESSIBLE : ACCESSIBLE,
+  install: NativeModules.MMKVNative.installMMKV
 }
 
 export default MMKVStorage;
