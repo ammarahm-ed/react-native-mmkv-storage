42d533ea63cbb2faab5ad52cbd9ffe6e0a094348
ammarahm-ed <ammarahmed6506@gmail.com>
2021-04-27 01:45:44 +0500

    defer calling installMMKV using setTimeout

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import { NativeModules } from 'react-native';
 import { ACCESSIBLE, MODES } from 'react-native-mmkv-storage/src/utils';
 if (!global.getStringMMKV) {
-  NativeModules.MMKVNative.installMMKV()
+    setTimeout(()=> NativeModules.MMKVNative.installMMKV(),1) 
 }
+
 const Loader = require('./src/loader').default;
 
 const MMKVStorage = {
   Loader:Loader,
   MODES: MODES,
   ACCESSIBLE : ACCESSIBLE,
-  install: NativeModules.MMKVNative.installMMKV
+  install:() => setTimeout(()=> NativeModules.MMKVNative.installMMKV(),1) 
 }
 
 export default MMKVStorage;
