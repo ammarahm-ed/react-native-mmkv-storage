6d0b7c3cb118514d9548898ccd4b474ea132e1d9
Chris Zelenak <chris@zelenak.me>
2021-06-09 11:34:54 -0400

    ensure API is exposed at module level

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import {useMMKVStorage as useMMKV} from "./src/hooks/useMMKV"
-import { ACCESSIBLE, MODES } from './src/utils';
+import { useMMKVStorage as useMMKV } from "./src/hooks/useMMKV";
+import { ACCESSIBLE, MODES } from "./src/utils";
 export const useMMKVStorage = useMMKV;
 export const create = require("./src/hooks/useMMKV").create;
 
-const Loader = require('./src/loader').default;
+const Loader = require("./src/loader").default;
+const API = require("./src/api").default;
 
 const MMKVStorage = {
-  Loader:Loader,
+  Loader: Loader,
+  API: API,
   MODES: MODES,
-  ACCESSIBLE : ACCESSIBLE,
-}
+  ACCESSIBLE: ACCESSIBLE,
+};
 
 export default MMKVStorage;
-
