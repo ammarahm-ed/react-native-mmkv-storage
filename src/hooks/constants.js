bce7ced556ae05daaaff118a9109c7a1b370b844
ammarahm-ed <ammarahmed6506@gmail.com>
2021-09-14 10:02:53 +0500

    rename method types & add copy function

--- src/hooks/constants.js
+++ src/hooks/constants.js
@@ -1,29 +1,38 @@
-export const types = ["string", "int", "bool", "map", "array"];
+export const types = ["string", "number", "boolean", "object", "array"];
 
 export const methods = {
   string: {
     indexer: "strings",
     get: "getString",
     set: "setString",
+    copy:value => value
   },
-  int: {
+  number: {
     indexer: "numbers",
     get: "getInt",
     set: "setInt",
+    copy:value => value
   },
-  bool: {
+  boolean: {
     indexer: "booleans",
     get: "getBool",
     set: "setBool",
+    copy:value => value
   },
-  map: {
+  object: {
     indexer: "maps",
     get: "getMap",
     set: "setMap",
+    copy:(value) => {
+      return {...value}
+    }
   },
   array: {
     indexer: "arrays",
     get: "getArray",
     set: "setArray",
+    copy:(value) => {
+      return [...value]
+    }
   },
 };
