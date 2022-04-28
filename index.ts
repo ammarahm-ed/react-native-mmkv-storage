c82d3359abba4bc74f9318e7b7882f56920f92d5
ammarahm-ed <ammarahmed6506@gmail.com>
2022-04-29 02:51:59 +0500

    refactor: undo changes

--- index.ts
+++ index.ts
@@ -1,27 +1,13 @@
-import { useMMKVStorage, create } from './src/hooks/useMMKV';
-import { ACCESSIBLE, MODES } from './src/utils';
-import { getCurrentMMKVInstanceIDs } from './src/initializer';
-import IDStore from './src/mmkv/IDStore';
 import { useIndex } from './src/hooks/useIndex';
-import { isLoaded, init } from './src/mmkv/init';
+import { create, useMMKVStorage } from './src/hooks/useMMKV';
+import { getCurrentMMKVInstanceIDs } from './src/initializer';
 import Loader from './src/loader';
-import API from './src/api';
+import IDStore from './src/mmkv/IDStore';
+import { init, isLoaded } from './src/mmkv/init';
 import mmkvJsiModule, { mmkvBridgeModule } from './src/module';
-import { MMKVJsiModule } from './src/types';
+import { ACCESSIBLE, MODES } from './src/utils';
 
-interface MMKVStorageInterface {
-  Loader: typeof Loader;
-  API?: API;
-  MODES: typeof MODES;
-  ACCESSIBLE: typeof ACCESSIBLE;
-  getAllMMKVInstanceIDs: typeof IDStore.getAllMMKVInstanceIDs;
-  getCurrentMMKVInstanceIDs: typeof getCurrentMMKVInstanceIDs;
-  IDSTORE_ID: string;
-  _jsiModule: MMKVJsiModule;
-  _bridgeModule: typeof mmkvBridgeModule;
-}
-
-const MMKVStorage: MMKVStorageInterface = {
+const MMKVStorage = {
   Loader: Loader,
   MODES: MODES,
   ACCESSIBLE: ACCESSIBLE,
@@ -34,4 +20,4 @@ const MMKVStorage: MMKVStorageInterface = {
 
 export default MMKVStorage;
 
-export { useMMKVStorage, create, useIndex, isLoaded, init, API, MMKVStorageInterface };
+export { useMMKVStorage, create, useIndex, isLoaded, init };
