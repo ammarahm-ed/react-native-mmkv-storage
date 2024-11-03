52d9aa97733529312047906a393edb861ac4dd37
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-11-03 12:56:32 +0500

    fix indexer issues

--- ios/MMKVStorage.h
+++ ios/MMKVStorage.h
@@ -1,13 +1,13 @@
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "MMKVStorageSpec.h"
-@interface MMKVNative : NSObject <NativeMMKVStorageSpec>
+@interface MMKVStorage : NSObject <NativeMMKVStorageSpec>
 @property (nonatomic, assign) BOOL setBridgeOnMainQueue;
 #else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
-@interface MMKVNative : NSObject <RCTBridgeModule>
+@interface MMKVStorage : NSObject <RCTBridgeModule>
 
 @property (nonatomic, assign) BOOL setBridgeOnMainQueue;
 #endif
