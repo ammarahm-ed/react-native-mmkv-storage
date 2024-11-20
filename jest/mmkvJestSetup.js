12766dddd173f9141e14dcf850b5ad5479e15cb8
Paul Schreiber <paulschreiber@gmail.com>
2024-11-20 11:27:46 -0500

    remove __turboModuleProxy from jest mock

--- jest/mmkvJestSetup.js
+++ jest/mmkvJestSetup.js
@@ -2,12 +2,6 @@ if (!global.__fbBatchedBridgeConfig) {
   global.__fbBatchedBridgeConfig = {};
 }
 
-if (!global.__turboModuleProxy) {
-  global.__turboModuleProxy = name => {
-    return {};
-  };
-}
-
 require('react-native').NativeModules.MMKVNative = {
   install: jest.fn(() => true)
 };
